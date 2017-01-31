895e9b9dd066742084c5197a6706f079852fdff8
ewancoder <ewancoder@gmail.com>
2017-01-31 09:52:31 +0300

    Add reusable busy session

--- src/busy.service.ts
+++ src/busy.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/Observable/of';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/mergeMap';
 
 @Injectable()
 export class BusyService {
@@ -12,6 +15,12 @@ export class BusyService {
 
     get isBusy$(): Observable<boolean> {
         return this.isBusy.asObservable();
+    }
+
+    busySession<T>(action: Observable<T>): Observable<T> {
+        return Observable.of(undefined).do(_ => this.busyOn())
+            .flatMap(_ => action)
+            .do(_ => this.busyOff(), _ => this.busyOff());
     }
 
     busyOn(): void {
